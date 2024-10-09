d8c9a9013e0844d14e3eada27ae863dfb78751ed
andrefrancisco <asfrancisco@ua.pt>
2024-10-09 14:32:05 +0100

    teste pag_nova

--- testeaulas/src/pages/nova_pag.js
+++ testeaulas/src/pages/nova_pag.js
@@ -5,8 +5,8 @@ export default function MinhaPagina() {
       <p>Este é o conteúdo da nova página.</p>
       <img src="/path-to-your-image.jpg" alt="Descrição da imagem" />
       <p>alo</p>
-      <p>alo</p>
-      <p>alo</p><p>alo</p><p>alo</p><p>alo</p><p>alo</p>
+      <p>alo</p><p>alo</p><p>alo</p><p>alo</p>
+   
     </div>
   );
 }
