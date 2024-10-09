4ca7e3d54fe61d8860367abc560bc22efc1024d1
andrefrancisco <asfrancisco@ua.pt>
2024-10-09 14:05:27 +0100

    yarn file

--- testeaulas/src/pages/nova_pag.js
+++ testeaulas/src/pages/nova_pag.js
@@ -1,9 +1,12 @@
 export default function MinhaPagina() {
-    return (
-      <div>
-        <h1>Minha Nova Página</h1>
-        <p>Este é o conteúdo da nova página.</p>
-        <img src="/path-to-your-image.jpg" alt="Descrição da imagem" />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Minha Nova Página</h1>
+      <p>Este é o conteúdo da nova página.</p>
+      <img src="/path-to-your-image.jpg" alt="Descrição da imagem" />
+      <p>alo</p>
+      <p>alo</p>
+      <p>alo</p>
+    </div>
+  );
+}
