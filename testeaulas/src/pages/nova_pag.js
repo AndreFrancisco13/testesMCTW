08073256da07148c4d67e01da3978f15c0a1ac78
andrefrancisco <asfrancisco@ua.pt>
2024-10-09 10:41:25 +0100

    nova pagina criada

---
+++ testeaulas/src/pages/nova_pag.js
@@ -0,0 +1,9 @@
+export default function MinhaPagina() {
+    return (
+      <div>
+        <h1>Minha Nova Página</h1>
+        <p>Este é o conteúdo da nova página.</p>
+        <img src="/path-to-your-image.jpg" alt="Descrição da imagem" />
+      </div>
+    );
+  }
